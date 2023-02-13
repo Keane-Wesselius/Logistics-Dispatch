d3b39d243b2936e37f79e961fa4f5c4dc1ad2f94
connergordon15 <connergordon15@hotmail.com>
2023-02-13 10:32:45 -0800

    first commit for login/register

---
+++ userauth/passport-config.js
@@ -0,0 +1,33 @@
+const localStrategy = require("passport-local").Strategy
+const bcrypt = require("bcrypt")
+
+
+function initialize(passport, getUserByEmail, getUserById) {
+    // Authenticate Users
+    const authUsers = async (email, password, done) => {
+        // Get user by email
+        const user = getUserByEmail(email)
+        if(user == null){
+            return done(null, false, {message: "No user found with that email"})
+        }
+        try {
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user)
+            } else {
+                return done(null, false, {message: "Password Incorrect"})
+            }
+        } catch (e) {
+            console.log(e)
+            return done(e)
+        }
+    }
+
+    passport.use(new localStrategy({usernameField: 'email'}, authUsers))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize
+
