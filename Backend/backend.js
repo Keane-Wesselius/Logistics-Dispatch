0720a3bf39ab24d7f81f0dbaab2812ca8ca005a3
wertomellon <Keanewesselius@gmail.com>
2023-02-17 22:39:33 -0800

    Mobile to backend skeleton and added backend functionality
    Map.js line 212: Commented out the line that was causing the error on android devices (Thanks Josh)
    
    Added functionality to the backend.js that allows connection to the database as well as to be able to call functions from the websocket
    
    Improved the functionality of the Expo websocket example to call the function from the backend, this will serve as an example to be used in the expo apps going forward.

--- Backend/backend.js
+++ Backend/backend.js
@@ -1,8 +1,28 @@
 const WebSocket = require("ws");
 const PacketDefinitions = require("./packet_definitions.js");
 const wss = new WebSocket.WebSocketServer({ port: 5000 });
+const { MongoClient } = require('mongodb');
+
+const uri = "MONGO KEY HERE";
+const dbClient = new MongoClient(uri);
 
 authenticated_clients = [];
+
+//Creates a connection to the database
+async function startDatabase(){
+	try {
+	  await dbClient.connect();
+	} 
+	catch (e) {
+	  console.error(e);
+	} 
+	// finally {
+	//   await client.close();
+	// }
+};
+startDatabase();
+
+
 
 // Create a new connection method with access to the active WebSocket connection.
 wss.on("connection", function connection(ws) {
@@ -26,10 +46,46 @@ wss.on("connection", function connection(ws) {
 				console.log("Authentication Data: " + data);
 			}
 
+			//Now that we have a JSON from the frontend we can check if we are trying to call a function
+			if (jsonObject.hasOwnProperty("function"))
+			{
+				if (jsonObject.function == "findIfUserExists")
+				{
+					findIfUserExists(dbClient, jsonObject.email, jsonObject.password)
+					//in this case authenticated is the value returned by findIfUserExists()
+  					.then(authenticated => {
+    					ws.send(authenticated);
+  					})
+  					.catch(err => {
+    					console.error(err);
+    					ws.send('Internal server error');
+  					});
+				}
+			}
+			
+
 			console.log("Got valid JSON object: " + jsonObject);
+			console.log("JSON values = " + JSON.stringify(jsonObject));
 			ws.send("From backend to desktop");
 		} else {
 			console.log("Got invalid JSON object");
 		}
 	});
 });
+
+
+//This function looks into the database and finds if an email and password exists in the database
+//If it does we return true and false if not
+//Currently I (keane) need to figure out bCrypt so i am passing the encrypted version of the password to get true values
+async function findIfUserExists(client, userEmail, userPassword) {
+	const result = await client.db("test").collection("users").findOne({ email: userEmail, password: userPassword });
+  
+	if (result) {
+		console.log(`User Credentials found in database!'${userEmail}':`);
+		//console.log(result);
+		return "true";
+	} else {
+		console.log(`User Credentials not in database'${userEmail}'`);
+		return "false";
+	}
+  };
--- examples/WebSocketExpoDemo/App.js
+++ examples/WebSocketExpoDemo/App.js
@@ -2,20 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
+//This React expo application is interfacing with Backend/backend.js
+
+//We need extends React.Component to use the correct websocket vs using import react-native-websocket
 export default class App extends React.Component {
 	componentDidMount() {
 		// This refers to a React WebSocket, not a browser WebSocket, which are different.
 		let ws = new WebSocket('ws://127.0.0.1:5000/');
+		//onopen happens when the websocket connects
 		ws.onopen = () => {
-			ws.send("TEST");
+			//ws.send sends data to backend.js 
+			//We MUST send a string and not a JSON to the backend
+			ws.send(JSON.stringify({function: "findIfUserExists", email: 'test@gmail.com', password: '$2b$10$WiSov1jp8GHWjEMAf7rFaejT7NHgbC9VBDLGQcO27.VqxKWnBtiJa' }));
 		}
 		ws.onclose = () => console.log('ws closed');
 
+		//When we get an answer back this is called 
 		ws.onmessage = e => {
-			const message = JSON.parse(e.data);
+			//JSON.parse is not working with the data sent back by the backend
+			const message = e;//JSON.parse(e);
+			//whatever we send back from the backend is a JSON with a bunch of weird things
+			//However what we send from the backend is stored in the data key of the JSON
+			console.log(e.data);
 			console.log('e', message);
 		};
+
+		ws.onerror = (e) => {
+			console.error('WebSocket error:', e.message);
+		};
 	}
+	
 
 	render() {
 		return (
