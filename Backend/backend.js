52c8e05154950f867f42f74254e3c84b4f4e4b74
Gamme99 <galmogalamso@gmail.com>
2023-02-18 16:40:53 -0800

    moved packet in mobile

--- Backend/backend.js
+++ Backend/backend.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 const Packets = require("../Common/packets.js");
-const wss = new WebSocket.WebSocketServer({ port: 5000 });
+const wss = new WebSocket.WebSocketServer({ port: 5005 });
 
 // Controls whether the system will try to interface with the database, disable for easier debugging / unrelated implementations.
 const doDatabase = false;
@@ -10,98 +10,106 @@ let uri = null;
 let dbClient = null;
 
 if (doDatabase) {
-	const { MongoClient } = require('mongodb');
-	uri = "MONGO KEY HERE";
-	dbClient = new MongoClient(uri);
+  const { MongoClient } = require("mongodb");
+  uri = "MONGO KEY HERE";
+  dbClient = new MongoClient(uri);
 }
 
 const authenticatedClients = [];
 
 if (doDatabase) {
-	// Creates a connection to the database
-	async function startDatabase() {
-		try {
-			await dbClient.connect();
-		}
-		catch (e) {
-			console.error(e);
-		}
-		// finally {
-		//   await client.close();
-		// }
-	};
-	startDatabase();
+  // Creates a connection to the database
+  async function startDatabase() {
+    try {
+      await dbClient.connect();
+    } catch (e) {
+      console.error(e);
+    }
+    // finally {
+    //   await client.close();
+    // }
+  }
+  startDatabase();
 }
 
 // Create a new connection method with access to the active WebSocket connection.
 wss.on("connection", function connection(ws) {
-	// If an error occurs in this connection. print to the console the error.
-	ws.on("error", console.error);
+  // If an error occurs in this connection. print to the console the error.
+  ws.on("error", console.error);
 
-	// Handles receiving a message from the current connection, with the data is in a buffer.
-	ws.on("message", function message(data) {
-		console.log("Raw Received Data: " + data);
+  // Handles receiving a message from the current connection, with the data is in a buffer.
+  ws.on("message", function message(data) {
+    console.log("Raw Received Data: " + data);
 
-		// TODO: Keeping the connection alive by permanently storing it?
-		if (!authenticatedClients.includes(ws)) {
-			// Attempt to parse any packet receive as a Login packet, as that is the required first packet for clients.
-			// TODO: If the client fails to send a valid login packet after a few attempts, they should be blocked from connecting for a period to avoid DOS attacks.
-			const loginPacket = Packets.LoginPacket.fromJSONString(data);
+    // TODO: Keeping the connection alive by permanently storing it?
+    if (!authenticatedClients.includes(ws)) {
+      // Attempt to parse any packet receive as a Login packet, as that is the required first packet for clients.
+      // TODO: If the client fails to send a valid login packet after a few attempts, they should be blocked from connecting for a period to avoid DOS attacks.
+      const loginPacket = Packets.LoginPacket.fromJSONString(data);
 
-			if (loginPacket.username != null && loginPacket.password != null) {
-				console.log("Got valid login: " + loginPacket.username + " " + loginPacket.password);
-				authenticatedClients.push(ws);
-			} else {
-				console.log("Got invalid login");
-			}
-		} else {
-			// The user in this case has already been authenticated, so any packets they send can be received and processed by the server.
+      if (loginPacket.username != null && loginPacket.password != null) {
+        console.log(
+          "Got valid login: " +
+            loginPacket.username +
+            " " +
+            loginPacket.password
+        );
+        authenticatedClients.push(ws);
+      } else {
+        console.log("Got invalid login");
+      }
+    } else {
+      // The user in this case has already been authenticated, so any packets they send can be received and processed by the server.
 
-			// TODO: Requiring some kind of token to be sent with each packet from a client to the server might be a good additional security measure, at the cost of more network traffic.
-			// Attempt to parse the packet type from the data.
+      // TODO: Requiring some kind of token to be sent with each packet from a client to the server might be a good additional security measure, at the cost of more network traffic.
+      // Attempt to parse the packet type from the data.
 
-			// TODO: This parsing required getting the JSON JavaScript object to check the type parameter. At the very least, this will check if the JSON is valid, but that is already being done when parsing fromJsonString(). Slightly inefficient, so maybe having a way to construct a Packet from a JSON JavaScript might want to be looked into.
-			const packetType = Packets.getPacketType(data);
-			if (packetType == Packets.PacketTypes.FIND_IF_USER_EXISTS) {
-				const findIfUserExistsPacket = Packets.DoesUserExistPacket.fromJSONString(data);
+      // TODO: This parsing required getting the JSON JavaScript object to check the type parameter. At the very least, this will check if the JSON is valid, but that is already being done when parsing fromJsonString(). Slightly inefficient, so maybe having a way to construct a Packet from a JSON JavaScript might want to be looked into.
+      const packetType = Packets.getPacketType(data);
+      if (packetType == Packets.PacketTypes.FIND_IF_USER_EXISTS) {
+        const findIfUserExistsPacket =
+          Packets.DoesUserExistPacket.fromJSONString(data);
 
-				// Check if the jsonObject is valid. By this point, we should be able to assume the JSON object is both safe and valid.
-				if (findIfUserExistsPacket != null) {
-					console.log("FIND USER EXISTS");
-					if (doDatabase) {
-						findIfUserExists(dbClient, jsonObject.email, jsonObject.password)
-							//in this case authenticated is the value returned by findIfUserExists()
-							.then(authenticated => {
-								ws.send(authenticated);
-							})
-							.catch(err => {
-								console.error(err);
-								ws.send('Internal server error');
-							});
-					}
-				} else {
-					console.log("Got invalid JSON object: " + data);
-				}
-			}
-		}
-	});
+        // Check if the jsonObject is valid. By this point, we should be able to assume the JSON object is both safe and valid.
+        if (findIfUserExistsPacket != null) {
+          console.log("FIND USER EXISTS");
+          if (doDatabase) {
+            findIfUserExists(dbClient, jsonObject.email, jsonObject.password)
+              //in this case authenticated is the value returned by findIfUserExists()
+              .then((authenticated) => {
+                ws.send(authenticated);
+              })
+              .catch((err) => {
+                console.error(err);
+                ws.send("Internal server error");
+              });
+          }
+        } else {
+          console.log("Got invalid JSON object: " + data);
+        }
+      }
+    }
+  });
 });
 
 // TODO: These might want to be split off into their own file to reduce the clutter in this file. Not sure how we want to structure the Backend server yet though.
 if (doDatabase) {
-	//This function looks into the database and finds if an email and password exists in the database
-	//If it does we return true and false if not
-	//Currently I (keane) need to figure out bCrypt so i am passing the encrypted version of the password to get true values
-	async function findIfUserExists(client, userEmail, userPassword) {
-		const result = await client.db("test").collection("users").findOne({ email: userEmail, password: userPassword });
+  //This function looks into the database and finds if an email and password exists in the database
+  //If it does we return true and false if not
+  //Currently I (keane) need to figure out bCrypt so i am passing the encrypted version of the password to get true values
+  async function findIfUserExists(client, userEmail, userPassword) {
+    const result = await client
+      .db("test")
+      .collection("users")
+      .findOne({ email: userEmail, password: userPassword });
 
-		if (result) {
-			console.log(`User Credentials found in database!'${userEmail}':`);
-			//console.log(result);
-			return "true";
-		} else {
-			console.log(`User Credentials not in database'${userEmail}'`);
-			return "false";
-		}
-	};
+    if (result) {
+      console.log(`User Credentials found in database!'${userEmail}':`);
+      //console.log(result);
+      return "true";
+    } else {
+      console.log(`User Credentials not in database'${userEmail}'`);
+      return "false";
+    }
+  }
 }
---
+++ Mobile/screens/packets.js
@@ -0,0 +1,135 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.Packets = {}));
+})(this, (function (exports) { 'use strict';
+
+	// Essentially, Rollup 'compiles' a JavaScript module which can be used in both Node and the browser (Expo), which will be required for this project and utilizes Rollup ( https://rollupjs.org )
+
+	// Contains constant names for JSON tags.
+	const Constants = {
+		USERNAME: "username",
+		EMAIL: "email",
+		PASSWORD: "password",
+		AREA: "area",
+		TYPE: "type",
+	};
+
+	// Contains function names, essentially Packet types.
+	const PacketTypes = {
+		LOGIN: "login",
+		GET_ACTIVE_JOBS: "get_active_jobs",
+		FIND_IF_USER_EXISTS: "find_if_user_exists",
+	};
+
+	// Helper Functions
+	function tryGet(object, field) {
+		try {
+			return object[field];
+		} catch (ignored) {
+		}
+
+		return null;
+	}
+
+	// Helper method for when / if we need more security / data sanitization in the future.
+	function parseJSON(jsonString) {
+		try {
+			// jsonString, if passed from the 'data' in WebSockets, might not actually be a string, so call the toString method to ensure it is.
+			// TODO: Ensure toString() is secure.
+			jsonString = jsonString.toString();
+
+			// 1000 characters is kinda an arbitrary limit, but it should prevent some attacks from receiving a large string which requires many CPU cycles to parse, resulting in a DOS attack.
+			if (jsonString != null && jsonString.length <= 1000) {
+				return JSON.parse(jsonString);
+			}
+		} catch (ignored) {
+		}
+
+		return null;
+	}
+
+	function getPacketType(jsonString) {
+		const jsonObject = parseJSON(jsonString);
+		return tryGet(jsonObject, Constants.TYPE);
+	}
+	// End Helper Functions
+
+	// Packet classes have two primary functions: converting from JSON into a valid, secure JavaScript object and converting back into JSON to be sent over WebSockets.
+	// When parsed from JSON, the returned JavaScript object should be validated to ensure no unsanitized data is allowed into the system. By the point the variable is set in the object, the data MUST be able to be assumed to be fully sanitized and safe.
+	class Packet {
+		constructor(type) {
+			this.type = type;
+		}
+
+		// Overrides the base toString() method, which is desirable for our application.
+		toString() {
+			try {
+				return JSON.stringify(this);
+			} catch (ignored) {
+			}
+
+			return null;
+		}
+
+		static fromJSONString(jsonString) {
+			const jsonObject = parseJSON(jsonString);
+			return new Packet(tryGet(jsonObject, Constants.type));
+		}
+	}
+
+	class LoginPacket extends Packet {
+		constructor(username, password) {
+			super(PacketTypes.LOGIN);
+
+			// TODO: Sanitize
+			this.username = username;
+			this.password = password;
+		}
+
+		static fromJSONString(jsonString) {
+			const jsonObject = parseJSON(jsonString);
+			return new LoginPacket(tryGet(jsonObject, Constants.USERNAME), tryGet(jsonObject, Constants.PASSWORD));
+		}
+	}
+
+	// TODO: Not an actual implementation, but shows how the schemes should work.
+	class GetActiveJobsPacket extends Packet {
+		constructor(area) {
+			super(PacketTypes.GET_ACTIVE_JOBS);
+
+			// TODO: Sanitize
+			this.area = area;
+		}
+
+		static fromJSONString(jsonString) {
+			const jsonObject = parseJSON(jsonString);
+			return new GetActiveJobsPacket(tryGet(jsonObject, Constants.AREA));
+		}
+	}
+
+	class DoesUserExistPacket extends Packet {
+		constructor(email, password) {
+			super(PacketTypes.FIND_IF_USER_EXISTS);
+
+			// TODO: Sanitize
+			this.email = email;
+			// TODO: Does finding if a user exists need to have password included? Authorization should likely be checked for the connection rather than the password.
+			this.password = password;
+		}
+
+		static fromJSONString(jsonString) {
+			const jsonObject = parseJSON(jsonString);
+			return new GetActiveJobsPacket(tryGet(jsonObject, Constants.email), tryGet(jsonObject, Constants.password));
+		}
+	}
+
+	exports.Constants = Constants;
+	exports.DoesUserExistPacket = DoesUserExistPacket;
+	exports.GetActiveJobsPacket = GetActiveJobsPacket;
+	exports.LoginPacket = LoginPacket;
+	exports.PacketTypes = PacketTypes;
+	exports.getPacketType = getPacketType;
+	exports.parseJSON = parseJSON;
+
+}));
