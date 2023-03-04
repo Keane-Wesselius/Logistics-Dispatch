abd1d30db794172b4bcdb2a6a66d61932bfd72d4
connergordon15 <connergordon15@hotmail.com>
2023-03-03 16:09:18 -0800

    Quick fix of cart items not loading

--- Desktop/load-up/src/views/merchant/Cart.js
+++ Desktop/load-up/src/views/merchant/Cart.js
@@ -77,7 +77,7 @@ class TableRow extends Component {
         var row = this.props.rowContent;
 
         if (Object.hasOwn(row, '_id') && Object.hasOwn(row, 'itemName') && Object.hasOwn(row, 'description') && Object.hasOwn(row, 'quantity') &&
-            Object.hasOwn(row, 'price') && Object.hasOwn(row, 'weight') && Object.hasOwn(row, 'supplierID') && Object.hasOwn(row, 'postedDate')) {
+            Object.hasOwn(row, 'price') && Object.hasOwn(row, 'weight') && Object.hasOwn(row, 'supplierId') && Object.hasOwn(row, 'postedDate')) {
             return (
                 <tr>
                     <td>{row._id}</td>
@@ -86,7 +86,7 @@ class TableRow extends Component {
                     <td>{row.quantity}</td>
                     <td>{row.price}</td>
                     <td>{row.weight}</td>
-                    <td>{row.supplierID}</td>
+                    <td>{row.supplierId}</td>
                     <td>{row.postedDate}</td>
                     <td>
                         <button className="cart" onClick={this.handleRemove}>
